bde17d8a9efc62d688f38780e48d204d2dc476a8
Kush Sharma <kush.73888@gmail.com>
2023-12-25 12:16:40 +0530

    hidden and flex cannot be together

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -48,7 +48,7 @@ const Hero = () => {
       </div>
       <div className='relative flex flex-1 items-start'>
 
-  <div className='hidden lg:flex z-20 flex w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8'>
+  <div className='lg:flex z-20 flex w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8'>
   
     <div className='flex flex-col'>
 
