e33fef8e65fdb0a513b1f8a25c8183390c339609
Kush Sharma <111528483+Kushhhhhhhh@users.noreply.github.com>
2024-07-24 23:40:45 +0530

    Updated Guide.tsx

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -6,7 +6,7 @@ const Guide = () => {
       <div className='padding-container max-container w-full pb-24'>
        <Image  src='/camp.svg' alt='camp' width={50} height={50}/>
        <p className='uppercase regular-18 -mt-1 mb-3 text-green-50'>
-        We are herre for you
+        We are here for you
        </p>
        <div className='flex flex-wrap justify-between gap-5 lg:gap-10'>
          <h2 className='bold-40 lg:bold-64 xl:max-w-[390px]'>Guide you to easy path</h2>
